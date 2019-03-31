34ac0736f5b6c9921362650651161c129e638876
YoheiIguchi <yiguchi@r.recruit.co.jp>
2019-04-01 07:27:49 +0900

    vim

--- .vim/template/cpp/base-compe.cpp
+++ .vim/template/cpp/base-compe.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 #define FOR(i, a, b) for (int i = (a); i < (b); ++i)
 #define REP(i, n) for (int i = 0; i < (n); ++i)
-#define print(T& x) { cout << x << `\n`; }
 
 int main() {
-	return 0; 
+
+	return 0;
 }
 
