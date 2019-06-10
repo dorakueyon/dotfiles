438a645598594d907bacc05413704a79258bb3bb
YoheiIguchi <yiguchi@r.recruit.co.jp>
2019-06-10 22:55:51 +0900

    c++ template

--- .vim/template/cpp/base-compe.cpp
+++ .vim/template/cpp/base-compe.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 #define FOR(i, a, b) for (int i = (a); i < (b); ++i)
 #define REP(i, n) for (int i = 0; i < (n); ++i)
+#define RREP(i, n) for (int i = n - 1; i >= 0; --i)
+#define INF 100000000
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+
+typedef long long ll;
 
 int main() {
 
