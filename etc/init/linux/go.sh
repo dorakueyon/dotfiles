c0b144582dfddb0b34b9f552805b5c00ba30b7d8
YoheiIguchi <yiguchi@r.recruit.co.jp>
2019-12-05 23:38:30 +0900

    go

--- etc/init/linux/go.sh
+++ etc/init/linux/go.sh
@@ -29,65 +29,30 @@ case "$(get_os)" in
 
   # Case of Linux
   linux)
-  uri="https://storage.googleapis.com/golang/go1.13.5.linux-amd64.tar.gz"
+    if has "yum"; then
+      log_echo "Install go with Yellowdog Updater Modified"
+      sudo yum -y install go
+    elif has "apt-get"; then
+        log_echo "Install go with Advanced Packaging Tool"
+        sudo apt-get -y install go
+    elif has "apk"; then
+        log_echo "Install go with apk"
+        sudo apk add --no-cache go
+	elif has "pacman"; then
+         log_echo "Install go with pacman"
+		pacman -S --noconfirm go
+     else
+         log_fail "error: require: YUM, APT, APK or PACMAN"
+         exit 1
+     fi
+     ;;
 
-  tar="${uri##*/}"
-  grt="/usr/local/go"
-  dir="go"
-
-  # exit with true if you have go command
-  if has "go"; then
-    log_pass "go: already installed"
-    exit
-  fi
-
-  if has "wget"; then
-    dler="wget"
-  elif has "curl"; then
-    dler="curl -O"
-  else
-    log_fail "error: require: wget or curl" 1>&2
-    exit 1
-  fi
-
-  # Cleaning
-  command mv -f "$grt"{,.$$} 2>/dev/null && :
-  command mv -f "$tar"{,.$$} 2>/dev/null && :
-  command mv -f "$dir"{,.$$} 2>/dev/null && :
-
-  # Downloading
-  log_echo "Downloading golang..."
-  eval "$dler" "$uri"
-  if [ $? -eq 0 -a -f "$tar" ]; then
-    log_echo "Unzip $tar ..."
-    tar xzf "$tar"
-  else
-    log_fail "error: failed to download from $uri" 1>&2
-    exit 1
-  fi
-
-  if [ ! -d "$dir" ]; then
-    log_fail "error: failed to expand $dir directory" 1>&2
-    exit 1
-  fi
-
-  # Installing
-  sudo cp -f -v "$dir"/bin/go "${PATH%%:*}"
-  sudo mv -f -v "$dir" "$grt"
-  sudo rm -f -v "$tar"
-
-  # Result
-  if eval "${PATH%%:*}/go version"; then
-    log_pass "go: installed successfully"
-  else
-    log_fail "go: failed to install golang"
-    exit 1
-  fi
-  ;;
-
-  # Other platforms such as BSD are supported
+     # Other platforms such as BSD are supported
   *)
   log_fail "error: this script is only supported osx and linux"
   exit 1
   ;;
+
 esac
+
+log_pass "go: installed successfully"
