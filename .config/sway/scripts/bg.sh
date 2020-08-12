12b17b5ca248507f53ef94bfa0de784df304cb49
YoheiIguchi <matthew.at.muse@gmail.com>
2020-08-12 18:06:17 +0900

    sway

--- .config/sway/scripts/bg.sh
+++ .config/sway/scripts/bg.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 wallpapers_path=$HOME/Images/wallpapers
+width=3840
+height=2160
 
-curl --output $wallpapers_path/random.jpg -L https://source.unsplash.com/random/2048x1152
+curl --output $wallpapers_path/random.jpg -L https://source.unsplash.com/random/${width}x${height}
