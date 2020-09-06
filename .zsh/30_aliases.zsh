a9e57186c97c897c8eccd0ef4aa829406dee5173
YoheiIguchi <matthew.at.muse@gmail.com>
2020-09-06 16:54:10 +0900

    exa

--- .zsh/30_aliases.zsh
+++ .zsh/30_aliases.zsh
@@ -36,16 +36,16 @@ alias ...='cd ../..'
 alias ls="exa -a --color=always --group-directories-first"
 alias l.='exa -a | egrep "^\."'
 alias ll="exa -l"
-alias ld='exa -ld'          # Show info about the directory
-alias lla='exa -laF'        # Show hidden all files
-alias ll='exa -lF'          # Show long file information
-alias la='exa -aF'          # Show hidden files
-alias lx='exa -lXB'         # Sort by extension
-alias lk='exa -lSr'         # Sort by size, biggest last
-alias lc='exa -ltcr'        # Sort by and show change time, most recent last
-alias lu='exa -ltur'        # Sort by and show access time, most recent last
-alias lt='exa -ltr'         # Sort by date, most recent last
-alias lr='exa -lR'          # Recursive ls
+alias ld='exa -ld'              # Show info about the directory
+alias lla='exa -laF'            # Show hidden all files
+alias ll='exa -lF'              # Show long file information
+alias la='exa -aF'              # Show hidden files
+alias lx='exa -lXB'             # Sort by extension
+alias lk='exa -lSr'             # Sort by size, biggest last
+alias lc='exa -ltcr'            # Sort by and show change time, most recent last
+alias lu='exa -ltur'            # Sort by and show access time, most recent last
+alias lt='exa -l --sort oldest' # Sort by date, most recent last
+alias lr='exa -lR'              # Recursive ls
 
 alias cp="${ZSH_VERSION:+nocorrect} cp -i"
 alias mv="${ZSH_VERSION:+nocorrect} mv -i"
